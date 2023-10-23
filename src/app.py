9aa299303dc6be78a512661552a2269b8e370bc7
SaraPe <135864591+sarap29@users.noreply.github.com>
2023-10-23 20:07:37 +0000

    se añade metodo POST

--- src/app.py
+++ src/app.py
@@ -30,13 +30,38 @@ def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
-
+    response_body = members
     return jsonify(response_body), 200
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    body = request.get_json(silent=True)
+    if body is None:
+        return jsonify ({'msg': 'Debes enviar información en el body'}), 400
+    if 'first_name' not in body:
+        return jsonify ({'msg': 'El campo first_name es requerido'}), 400
+    if 'age' not in body:
+        return jsonify ({'msg': 'El campo age es requerido'}), 400
+    if 'lucky_numbers' not in body:
+        return jsonify ({'msg': 'El campo lucky_numbers es requerido'}), 400
+    id_member = jackson_family._generateId()
+    if 'id_member' in body:
+        id_member = body['id_member']
+    new_member = {
+        'id' : id_member,
+        'first_name': body['first_name'],
+        'last_name' : jackson_family.last_name,
+        'age': body['age'],
+        'lucky_numbers': body['lucky_numbers']
+    }
+    jackson_family.add_member(new_member)
+    return jsonify({'new_member':new_member}),200
+
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member(id):
+    member = jackson_family.get_member(id)
+    
+    
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
